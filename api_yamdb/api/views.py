c6c1b41063a697441a9efccdf1980376e339a0a5
Мария Кислякова <viridis.ber@yandex.ru>
2024-01-29 23:23:06 +0300

    ИСПРАВЛЕНА РОЛЬ

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -110,8 +110,7 @@ class UsersViewSet(viewsets.ModelViewSet):
         )
         if serializer.is_valid(raise_exception=True):
             if self.request.method == "PATCH":
-                serializer.validated_data.pop("role", None)
-            serializer.save()
+                serializer.save(role=self.request.user.role)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
